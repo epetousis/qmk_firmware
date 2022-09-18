f80b91e679d7d82c15bfc913361403ac45871b73
Evan Petousis <evan@petousis.net>
2022-09-18 21:01:55 +1000

    ut472-epetousis: rebind menu to AltGr

--- keyboards/ut472/keymaps/epetousis/keymap.c
+++ keyboards/ut472/keymaps/epetousis/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------------------------------------------------------------------------+
    * | Shift |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  '  |
    * |-------------------------------------------------------------------------+
-   * | Ctrl| Alt | Gui | App |  L2  |   Space   |  L1  | Left| Down|  Up |Right|
+   * | Ctrl| Alt | Gui |AltGr|  L2  |   Space   |  L1  | Left| Down|  Up |Right|
    * `-------------------------------------------------------------------------'
    */
 
@@ -52,7 +52,7 @@ LAYOUT(
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   MTCTRL_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_SFTENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-  KC_LCTL, KC_LALT, KC_LGUI, KC_APP,  MO(2),      KC_SPC,        MO(1),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LCTL, KC_LALT, KC_LGUI, KC_ALGR,  MO(2),      KC_SPC,        MO(1),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
   /* FN Layer 1
